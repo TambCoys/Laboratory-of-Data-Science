529a5eccc5932ac94914340f8cc494a5f900c39e
Habrikio <anellifabrizio13@gmail.com>
2025-12-05 19:40:05 +0100

    Update BridgeTable.sql

--- SQL_Tables/BridgeTable.sql
+++ SQL_Tables/BridgeTable.sql
@@ -8,7 +8,7 @@ CREATE TABLE dbo.Feats_Bridge (
     Feat_Artist_Code_FK    INT NOT NULL,
     Feats_Code_FK    INT NOT NULL,  
 
-    CONSTRAINT PK_Fact PRIMARY KEY (Feat_Artist_Code_FK, Feats_Code_FK)
+    CONSTRAINT PK_Bridge PRIMARY KEY (Feat_Artist_Code_FK, Feats_Code_FK)
 );
 GO
 
@@ -24,4 +24,5 @@ ALTER TABLE dbo.Feats_Bridge
 ADD CONSTRAINT FK_Bridge_Feats
     FOREIGN KEY (Feats_Code_FK)
     REFERENCES dbo.Dim_Feats(Feats_Code_PK);
+
 GO
