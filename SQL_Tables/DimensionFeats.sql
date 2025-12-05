77109d6f5427fcc75b640faa4f6584c3f3520fa1
TambCoys <marcotambe01@gmail.com>
2025-12-05 17:00:41 +0100

    Corretta per identity not null

--- SQL_Tables/DimensionFeats.sql
+++ SQL_Tables/DimensionFeats.sql
@@ -1,6 +1,9 @@
 -- 7. Dim_Feats (Intermediate table per la bridge table)
-IF OBJECT_ID('dbo.Dim_Feats') IS NOT NULL DROP TABLE dbo.Dim_Feats;
+IF OBJECT_ID('dbo.Dim_Feats', 'U') IS NOT NULL 
+    DROP TABLE dbo.Dim_Feats;
+GO
+
 CREATE TABLE dbo.Dim_Feats (
-    Feats_Code_PK VARCHAR(10) IDENTITY(1,1) NOT NULL PRIMARY KEY
+    Feats_Code_PK VARCHAR(10) NOT NULL PRIMARY KEY
 );
 GO
