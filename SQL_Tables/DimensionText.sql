7f92a2ea08829aafccc9c3fa44983eb7773925a2
TambCoys <marcotambe01@gmail.com>
2025-12-06 12:55:51 +0100

    lyrics varchar(2000) era troppo poco

--- SQL_Tables/DimensionText.sql
+++ SQL_Tables/DimensionText.sql
@@ -1,9 +1,11 @@
--- 5. Dim_Text_Category (lyrics – da Assignment 3)
-IF OBJECT_ID('dbo.Dim_Text') IS NOT NULL DROP TABLE Dim_Text;
+-- 5. Dim_Text_Category (lyrics â€“ da Assignment 3)
+IF OBJECT_ID('dbo.Dim_Text') IS NOT NULL
+    DROP TABLE dbo.Dim_Text;
+
 CREATE TABLE dbo.Dim_Text (
-    Text_Code_PK             INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    N_Swear_Words_IT         INT    NULL,   
-    N_Swear_Words_EN         INT    NULL,  
-    Is_Explicit              BIT    NULL,  
-    Lyrics                   NVARCHAR(2000) NULL
+    Text_Code_PK       INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    N_Swear_Words_IT   INT        NULL,
+    N_Swear_Words_EN   INT        NULL,
+    Is_Explicit        BIT        NULL,
+    Lyrics             NVARCHAR(MAX) NULL
 );
