cf5efedda9eccdf141ea518a6b66f6ecc27f6f16
Habrikio <anellifabrizio13@gmail.com>
2025-12-23 17:42:38 +0100

    Update hdbscan_clustering.py

--- Assignment_3/hdbscan_clustering.py
+++ Assignment_3/hdbscan_clustering.py
@@ -1,15 +1,14 @@
-"""
-A function to perform HDBScan clustering on a DataFrame. The dataframe is assumed normalized. The non-normalized
-dataframe is used only to append the labels resulting from the clustering. x_axis and y_axis are used for the scatter
-plot and need to be attributes of the non-normalized dataframe. The other inputs are connected to the clustering.
-"""
-
 from sklearn.cluster import HDBSCAN
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 def hdbscan_clust(df, df_not_norm, x_axis, y_axis, cluster_selection_epsilon=0.5,
                   min_samples=5, min_cluster_size=10, max_cluster_size=15):
+    """
+    A function to perform HDBScan clustering on a DataFrame. The dataframe is assumed normalized. The non-normalized
+    dataframe is used only to append the labels resulting from the clustering. x_axis and y_axis are used for the scatter
+    plot and need to be attributes of the non-normalized dataframe. The other inputs are connected to the clustering.
+    """
     hdb = HDBSCAN(cluster_selection_epsilon=cluster_selection_epsilon, min_samples=min_samples, 
                   min_cluster_size=min_cluster_size, max_cluster_size=max_cluster_size,
                   store_centers="centroid")
@@ -24,3 +23,4 @@ def hdbscan_clust(df, df_not_norm, x_axis, y_axis, cluster_selection_epsilon=0.5
     
 
     plt.show()
+
