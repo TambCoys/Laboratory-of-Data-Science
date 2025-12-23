138bd817926d3db43c892c0d0d1a694d8dd74ce7
Habrikio <anellifabrizio13@gmail.com>
2025-12-23 17:46:47 +0100

    Update k_means_clustering.py

--- Assignment_3/k_means_clustering.py
+++ Assignment_3/k_means_clustering.py
@@ -1,18 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 19 19:07:46 2025
-
-@author: Utente
-"""
-
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 def k_means_clust(df, df_not_norm, n_clust, n_init=20, max_iter=100, random_state=42, legend=False, hue='primary_artist'):
+    """
+    A function to perform K-means clustering on a DataFrame. The dataframe is assumed normalized. The non-normalized
+    dataframe is used only to append the labels resulting from the clustering. The other inputs are connected to the clustering.
+    The function also shows a countplot showing for each cluster how many element of a certain hue are there. For example,
+    for the default hue='primary_artist', the countplot shows in each cluster how many songs of each artist there are.
+    """
     kmeans = KMeans(n_clusters=n_clust, n_init=n_init, max_iter=max_iter, random_state=random_state)
     kmeans.fit(df)
     df_not_norm['kmeans_labels'] = kmeans.labels_
     fig = plt.figure(figsize=(13, 8))
     sns.countplot(data=df_not_norm, x='kmeans_labels', hue=hue, legend=False)
+
     plt.show()
