663a45439b49a26e03df19efb3f5069c27c3dade
Habrikio <anellifabrizio13@gmail.com>
2025-12-23 17:51:47 +0100

    Update script_assignment_3.py

--- Assignment_3/script_assignment_3.py
+++ Assignment_3/script_assignment_3.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 19 18:36:57 2025
-
-@author: Utente
-"""
-
 import json
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -18,33 +11,42 @@ with open('tracks_cleaned.json', encoding='utf-8') as f:
     tracks = json.load(f)
 df_tracks = pd.DataFrame(tracks)
 
+# Selecting the subset of rows to keep for the clustering, and then normalizing it
 to_keep = ['bpm', 'rolloff', 'flux', 'rms', 'flatness', 'spectral_complexity', 'pitch', 'loudness']
 scaler = StandardScaler()
 scaler.fit(df_tracks[to_keep])
 df_norm = scaler.transform(df_tracks[to_keep])
 
+# Hierarchical clustering
 hc.hierarchical_clust(df=df_norm, distance_threshold=10)
+# We see that single and average linkage provide one big cluster and several very small, not suited for our categorization. 
+# Complete linkage does a better job, but still not satisfactory
 
+# HDBScan clustering
 hdbc.hdbscan_clust(df=df_norm, df_not_norm=df_tracks, x_axis="pitch", y_axis="loudness",
                   cluster_selection_epsilon=0.5, min_samples=5)
+# Independently from the values in input, the clusters are not suited for our analysis
 
-df_tracks = df_tracks.drop(['hdbscan_labels'], axis=1)
+df_tracks = df_tracks.drop(['hdbscan_labels'], axis=1) # Dropping the HDBScan label, since we discarded that method
 
-km_choice.k_means_choice(df=df_norm, range_lower=2, range_upper=30)
+# K-Means clustering
+km_choice.k_means_choice(df=df_norm, range_lower=2, range_upper=30) # It appears that the best number of clusters is 7
 
 n_clust = 7
 
 kmc.k_means_clust(df=df_norm, df_not_norm=df_tracks, n_clust=n_clust)
 
+# Attributing names to clusters based on their characteristics
 label_to_cat = {0:'Minimal', 1:'Fast-Flow', 2:'Hype', 3:'Warm Bangers', 4:'Clean',
                 5:'Melodic', 6:'Slow Dark'}
 
 for i in range(n_clust):
     df = df_tracks.loc[df_tracks['kmeans_labels']==i, 'category'] = label_to_cat[i]
     
-
+# Updating and saving the dataset
 df_tracks_json = df_tracks.to_dict(orient='records')
 
 with open('tracks_cleaned.json', 'w', encoding='utf-8') as f:
 
     json.dump(df_tracks_json, f, indent=2, ensure_ascii=False)
+
