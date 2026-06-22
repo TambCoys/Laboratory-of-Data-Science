4db42b19019496d7ed23e2682b851e1ef0ee0a31
TambCoys <marcotambe01@gmail.com>
2026-06-22 21:09:14 +0200

    Remove project files and keep README only

--- CodiciAssignment7/Dim_Feats_SSIS.sql
+++
@@ -1,9 +0,0 @@
--- 7. Dim_Feats_SSIS (Intermediate table per la bridge table)
-IF OBJECT_ID('dbo.Dim_Feats_SSIS') IS NOT NULL 
-    DROP TABLE dbo.Dim_Feats_SSIS;
-GO
-
-CREATE TABLE dbo.Dim_Feats_SSIS (
-    Feats_Code_PK INT IDENTITY(1,1) NOT NULL PRIMARY KEY
-);
-GO
